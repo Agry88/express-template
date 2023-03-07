28d7fb8880b94c13788076385168040a51cea031
agry88 <rgok307085@gmail.com>
2023-03-07 11:56:40 +0800

    Fix: wrong migration file

---
+++ prisma/migrations/20230307035204_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Member` (
+    `member_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Member_email_key`(`email`),
+    PRIMARY KEY (`member_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
