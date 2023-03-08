87e5a5371bac6bee2e4c68b0ffeb5567962495f8
Agry <rgok307085@gmail.com>
2023-03-08 08:44:21 +0800

    Fix:delete useless config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,16 +3,12 @@ dotenv.config()
 
 export type Config = {
   port: number
-  emailUser: string
-  emailPassword: string
   hostURL: string
   jwtSecret: string
 }
 
 const config: Config = {
   port: process.env.PORT === undefined ? 3001 : Number(process.env.PORT),
-  emailUser: process.env.EMAIL_USER === undefined ? '' : process.env.EMAIL_USER,
-  emailPassword: process.env.EMAIL_PASSWORD === undefined ? '' : process.env.EMAIL_PASSWORD,
   hostURL: process.env.HOST_URL === undefined ? '' : process.env.HOST_URL,
   jwtSecret: process.env.JWT_SECRET === undefined ? '' : process.env.JWT_SECRET
 }
